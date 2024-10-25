6050072cfa7d5bf78029db5bfaf920dade8ff411
Yavor Shahpasov <yavosh@gmail.com>
2024-10-25 11:21:09 +0300

    Update time parsing

--- main.go
+++ main.go
@@ -2,6 +2,9 @@ package main
 
 import (
 	"bufio"
+	"encoding/json"
+	"fmt"
+	"io"
 	"os"
 	"strconv"
 	"strings"
@@ -20,6 +23,21 @@ func flusher(w flushable, d time.Duration) {
 		_ = w.Flush()
 	}
 }
+
+var (
+	fp  = color.New(color.FgWhite)
+	mp  = color.New(color.FgWhite, color.Bold)
+	epk = color.New(color.FgCyan, color.Faint)
+	epv = color.New(color.FgWhite, color.Faint)
+
+	skip = map[string]bool{
+		"logger":    true,
+		"level":     true,
+		"timestamp": true,
+		"time":      true,
+		"severity":  true,
+	}
+)
 
 func ts(t string) string {
 	if ux, err := strconv.ParseInt(t, 10, 64); err == nil {
@@ -48,6 +66,56 @@ func level(l string) *color.Color {
 	default:
 		return color.New(color.FgWhite, color.Faint)
 	}
+}
+
+func format(line string, w io.Writer) {
+	message := make(map[string]interface{})
+	err := json.Unmarshal([]byte(line), &message)
+
+	if err != nil {
+		_, _ = fp.Fprintf(w, line)
+		return
+	}
+
+	if v, ok := message["severity"]; ok {
+		lp := level(fmt.Sprintf("%s", v))
+		_, _ = fp.Fprintf(w, "[")
+		_, _ = lp.Fprintf(w, "%5s", v)
+		_, _ = fp.Fprintf(w, "] ")
+	}
+
+	if v, ok := message["level"]; ok {
+		lp := level(fmt.Sprintf("%s", v))
+		_, _ = fp.Fprintf(w, "[")
+		_, _ = lp.Fprintf(w, "%5s", v)
+		_, _ = fp.Fprintf(w, "] ")
+	}
+
+	if v, ok := message["timestamp"]; ok {
+		_, _ = fp.Fprintf(w, "%s ", ts(fmt.Sprintf("%s", v)))
+	}
+
+	if v, ok := message["time"]; ok {
+		_, _ = fp.Fprintf(w, "%s ", ts(fmt.Sprintf("%s", v)))
+	}
+
+	if v, ok := message["logger"]; ok {
+		_, _ = fp.Fprintf(w, "%-12s - ", v)
+	}
+
+	if v, ok := message["message"]; ok {
+		_, _ = mp.Fprintf(w, "%s ", v)
+	}
+
+	for k, v := range message {
+		if !skip[k] {
+			_, _ = epk.Fprintf(w, "%s", k)
+			_, _ = epv.Fprintf(w, "=")
+			_, _ = epv.Fprintf(w, "%v ", v)
+		}
+	}
+
+	_, _ = fp.Fprintf(w, "\n")
 }
 
 func main() {
